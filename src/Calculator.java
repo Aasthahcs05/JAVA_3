1cad0267f11574761bc4ecdc6cd9cb131429a215
Aastha_3 <aastha.2428cseai919@kiet.edu>
2025-08-27 23:49:57 +0530

    Add code

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,14 +5,21 @@ public class Calculator {
         Scanner sc=new Scanner(System.in);
         int x, y, z=0;
         System.out.println("Enter any two no. ");
-        x=sc.nextInt();
-        y=sc.nextInt();
+//        x=sc.nextInt(); //to handle the exception while taking input take the input in try block not outside
+//        y=sc.nextInt();
         try
         {
+
+            x=sc.nextInt();
+            y=sc.nextInt();
             z=x/y;
         }
         //try can have multiple catch but from least specific to most specific
 
+        catch(NumberFormatException e)
+        {
+            System.out.println(e.getMessage());
+        }
       catch(Exception e)
         {
             System.out.println(e.getMessage());
