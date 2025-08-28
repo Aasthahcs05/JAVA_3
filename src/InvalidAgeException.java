7e95a6bc4b8a4bb037ee4ecb019fc0dade8810b9
Aastha_3 <aastha.2428cseai919@kiet.edu>
2025-08-28 09:43:54 +0530

    Add code

---
+++ src/InvalidAgeException.java
@@ -0,0 +1,9 @@
+//Step 1
+public class InvalidAgeException extends Exception {
+    public InvalidAgeException(String message)
+    //step 2
+     {
+        super(message);
+    }
+}
+
---
+++ src/UserDefinedException.java
@@ -0,0 +1,12 @@
+//step 2 contn...
+public class UserDefinedException {
+    public void validate(int age) throws InvalidAgeException
+    {
+        if(age<18)
+            throw new InvalidAgeException("Not eligible to vote age less than 18");
+        else
+            System.out.println("Eligible to vote");
+    }
+
+
+}
